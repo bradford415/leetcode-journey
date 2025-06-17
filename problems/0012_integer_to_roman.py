ff8d3be82eff820eaec82c49b3c9caaa0a172d0e
Bradley Selee <bselee415@gmail.com>
2025-06-16 21:54:49 -0600

    finished int to roman

--- problems/0012_integer_to_roman.py
+++ problems/0012_integer_to_roman.py
@@ -3,46 +3,53 @@ from typing import Optional
 
 def intToRoman(num: int) -> str:
     """
-    time complexity:
-    space complexity:
+    time complexity: O(1)
+        - we only need to loop over the map which is always a constant size and doesn't grow based
+        - on the num size
+    space complexity: O(1)
+        - this is only constant space since the problem contrains the num <= 3999 so there can be
+          only ~15 roman numers; if this was not capped then it would be O(n) I believe
     """
-    rn_map = {
-        1000: "M",
-        500: "D",
-        100: "C",
-        50: "L",
-        10: "X",
-        5: "V",
-        1: "I"
-    }
+    # Create a map of the given table and also add the special cases (900, 400, 90, 40, etc...)
+    # NOTE: I used a list of lists to preserve order but I found out dicts in python 3.7 guarantee 
+    rn_map = [
+        [1000,"M"],
+        [900,"CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"],
+    ]
 
+    # empty string to build the resulting string
     numeral = ""
 
-    temp_num = num
-    for key, val in rn_map.items():
-
-        digit = temp_num // key
-
-        while temp_num > 0:
-
-            if digit == 4:
-                breakpoint()
-                temp_num -= 4*key
-                numeral += rn_map[key]
-                numeral += rn_map[4*key+key]
-                break
-            elif digit == 9:
-                temp_num -= 9*key
-                numeral += rn_map[key]
-                numeral += rn_map[9*key+key]
-                break
-            elif temp_num - key >= 0:
-                temp_num -= key
-                numeral += rn_map[key]
-            else:
-                break
-
+    # Loop through the highest roman numerals first since that's how we represent roman numerals
+    for r_num, roman, in rn_map:
+        
+        # use integer division to figure out how many symbols we need 
+        # (e.g., 3794 // 1000 = 3 -> 3  Ms)
+        num_lettters = num // r_num
+        
+        # build the string for this position of the number 
+        # (the next 2nd iteration will be 794//900 = 0 so it will append an empty string 
+        # which does nothing, this is why we don't need to account for edge cases)
+        numeral += roman * num_lettters
+         
+        # use the remainder to update the number (3794 -> 794)
+        num %= r_num
+    
     return numeral
+         
+        
+
 
 if __name__ == "__main__":
     
