77d1cc9d41f6f071ebfbfd5c0c4793181b9d53f4
Bradley Selee <bselee415@gmail.com>
2025-07-20 12:00:26 -0600

    start lc 10 but need to finish

---
+++ problems/0010_regular_expression_matching.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+
+def isMatch(s: str, p: str):
+    """
+    time complexity:
+    space complexity:
+    """
+    # DNF; TODO: need to finish
+    
+    return
+
+
+
+if __name__ == "__main__":
+
+    # test cases
+
+    ret = isMatch()
+    print(ret)
