065ccca983a81ef864d8134bc5673cd809df8ada
Bradley <bselee415@gmail.com>
2025-10-31 18:58:52 -0600

    fixed memo

--- problems/0062_unique_paths.py
+++ problems/0062_unique_paths.py
@@ -2,10 +2,11 @@ from typing import List, Optional
 
 class Solution:
     
-    def uniquePaths(self, m, n)
+    def uniquePaths(self, m, n):
+        pass
     
     def uniquePaths_recursive_slow(self, m: int, n: int) -> int:
-        """Recursive solution with memoization but does NOT pass the time limit 
+        """Recursive solution with memoization
         time complexity:
         space complexity:
         """
@@ -24,8 +25,12 @@ class Solution:
             if pos_row >= m or pos_col >= n:
                 memo[(pos_row, pos_col)] = 0
                 return 0
+            
+            total_paths = move_bot(pos_row + 1, pos_col) +  move_bot(pos_row, pos_col + 1)
 
-            return move_bot(pos_row + 1, pos_col) +  move_bot(pos_row, pos_col + 1)
+            memo[(pos_row, pos_col)] = total_paths
+
+            return total_paths
 
         all_paths = move_bot(0, 0)
 
@@ -36,9 +41,10 @@ class Solution:
 if __name__ == "__main__":
 
     # test cases
-    case_1 = 5
+    case_m = 3
+    case_n = 7
     
     solution = Solution()
 
-    ret = solution.functionName(case_1)
+    ret = solution.uniquePaths_recursive_slow(case_m, case_n)
     print(ret)
