0585160441fb435ece7b9f9eed24b4da55cdec89
Bradley <bselee415@gmail.com>
2025-11-30 08:54:40 -0700

    lc 69

---
+++ problems/0069_sqrt.py
@@ -0,0 +1,36 @@
+from typing import List, Optional
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        """Binary search, if loop finishes return mid if mid*mid <= x else mid-1 since rounding down
+        time complexity:
+        space complexity:
+        """
+        left = 0
+        right = x
+
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            elif mid * mid > x:
+                right = mid - 1
+
+        if mid*mid > x:
+            return mid - 1
+        return mid
+
+
+
+if __name__ == "__main__":
+
+    # test cases
+    case_1 = 5
+    
+    solution = Solution()
+
+    ret = solution.mySqrt(case_1)
+    print(ret)
